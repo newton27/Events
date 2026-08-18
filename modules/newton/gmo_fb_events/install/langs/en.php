9574b228d797fb47ff6b536e6f9d2b1a64b6f16f
Michael Newton <mlnnflorida@gmail.com>
2026-08-18 03:17:01 -0400

    Remove unsupported PHP language resource

--- modules/newton/gmo_fb_events/install/langs/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$aLangContent = array(
-    '_gmo_fb_events' => 'Facebook Events Importer',
-    '_gmo_fb_events_err_access' => 'Administrator access is required.',
-    '_gmo_fb_events_err_config' => 'Complete the importer settings in Studio before importing.',
-);
-
