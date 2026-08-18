e7ad86672c92f2eccaa20aeaaf412a8f926d7534
Michael Newton <mlnnflorida@gmail.com>
2026-08-18 02:40:54 -0400

    Add Studio registration in modules/newton/gmo_fb_events/install/sql/uninstall.sql

--- modules/newton/gmo_fb_events/install/sql/uninstall.sql
+++ modules/newton/gmo_fb_events/install/sql/uninstall.sql
@@ -1,1 +1,7 @@
+SET @sName = 'gmo_fb_events';
+
+DELETE FROM `tp`, `tw`, `tpw`
+USING `sys_std_pages` AS `tp`, `sys_std_widgets` AS `tw`, `sys_std_pages_widgets` AS `tpw`
+WHERE `tp`.`id` = `tw`.`page_id` AND `tw`.`id` = `tpw`.`widget_id` AND `tp`.`name` = @sName;
+
 DROP TABLE IF EXISTS `gmo_fb_events_imports`;
