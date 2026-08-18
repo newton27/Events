17f89f8998ef618a9684a5486c9e62a3e211b069
Michael Newton <mlnnflorida@gmail.com>
2026-08-18 00:38:16 -0400

    Add modules/newton/gmo_fb_events/install/langs/en.php

---
+++ modules/newton/gmo_fb_events/install/langs/en.php
@@ -0,0 +1,8 @@
+<?php
+
+$aLangContent = array(
+    '_gmo_fb_events' => 'Facebook Events Importer',
+    '_gmo_fb_events_err_access' => 'Administrator access is required.',
+    '_gmo_fb_events_err_config' => 'Complete the importer settings in Studio before importing.',
+);
+
