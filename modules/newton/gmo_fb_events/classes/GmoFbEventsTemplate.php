a556b78ee505a8369d4119c679bea045a09f5a88
Michael Newton <mlnnflorida@gmail.com>
2026-08-18 01:05:00 -0400

    Add modules/newton/gmo_fb_events/classes/GmoFbEventsTemplate.php

---
+++ modules/newton/gmo_fb_events/classes/GmoFbEventsTemplate.php
@@ -0,0 +1,9 @@
+<?php defined('BX_DOL') or die('hack attempt');
+
+class GmoFbEventsTemplate extends BxDolModuleTemplate
+{
+    public function __construct(&$oConfig, &$oDb)
+    {
+        parent::__construct($oConfig, $oDb);
+    }
+}
