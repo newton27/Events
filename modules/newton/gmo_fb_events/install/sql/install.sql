4074519d2e01c0210f189b94b16da9f745e369cd
Michael Newton <mlnnflorida@gmail.com>
2026-08-18 03:20:56 -0400

    Add custom Studio icon in modules/newton/gmo_fb_events/install/sql/install.sql

--- modules/newton/gmo_fb_events/install/sql/install.sql
+++ modules/newton/gmo_fb_events/install/sql/install.sql
@@ -18,12 +18,12 @@ CREATE TABLE IF NOT EXISTS `gmo_fb_events_imports` (
 
 -- Studio page and dashboard widget.
 INSERT INTO `sys_std_pages` (`index`, `name`, `header`, `caption`, `icon`) VALUES
-(3, @sName, '_gmo_fb_events', '_gmo_fb_events', 'calendar-days');
+(3, @sName, '_gmo_fb_events', '_gmo_fb_events', 'gmo_fb_events@modules/newton/gmo_fb_events/|std-icon.svg');
 SET @iPageId = LAST_INSERT_ID();
 
 SET @iParentPageId = (SELECT `id` FROM `sys_std_pages` WHERE `name` = 'home');
 SET @iParentPageOrder = (SELECT MAX(`order`) FROM `sys_std_pages_widgets` WHERE `page_id` = @iParentPageId);
 INSERT INTO `sys_std_widgets` (`page_id`, `module`, `url`, `click`, `icon`, `caption`, `cnt_notices`, `cnt_actions`) VALUES
-(@iPageId, @sName, '{url_studio}module.php?name=gmo_fb_events', '', 'calendar-days', '_gmo_fb_events', '', 'a:4:{s:6:"module";s:6:"system";s:6:"method";s:11:"get_actions";s:6:"params";a:0:{}s:5:"class";s:18:"TemplStudioModules";}');
+(@iPageId, @sName, '{url_studio}module.php?name=gmo_fb_events', '', 'gmo_fb_events@modules/newton/gmo_fb_events/|std-icon.svg', '_gmo_fb_events', '', 'a:4:{s:6:"module";s:6:"system";s:6:"method";s:11:"get_actions";s:6:"params";a:0:{}s:5:"class";s:18:"TemplStudioModules";}');
 INSERT INTO `sys_std_pages_widgets` (`page_id`, `widget_id`, `order`) VALUES
 (@iParentPageId, LAST_INSERT_ID(), IF(ISNULL(@iParentPageOrder), 1, @iParentPageOrder + 1));
