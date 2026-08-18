98d4e7fca8526edba20e210649912a8fabff23f4
Michael Newton <mlnnflorida@gmail.com>
2026-08-18 01:04:57 -0400

    Fix UNA 15 installation for modules/newton/gmo_fb_events/install/sql/uninstall.sql

--- modules/newton/gmo_fb_events/install/sql/uninstall.sql
+++ modules/newton/gmo_fb_events/install/sql/uninstall.sql
@@ -1,4 +1,1 @@
 DROP TABLE IF EXISTS `gmo_fb_events_imports`;
-DELETE FROM `sys_options` WHERE `name` LIKE 'gmo_fb_events_%';
-DELETE FROM `sys_options_categories` WHERE `name` = 'Facebook Events Importer';
-
